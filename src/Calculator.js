6e6d95493129edeec658a66e918c0a140fbaa74c
Jana-script <jana.jscript@gmail.com>
2025-02-23 11:02:23 +0100

    commit 6
    closures in effects

--- src/Calculator.js
+++ src/Calculator.js
@@ -9,15 +9,6 @@ function Calculator({ workouts, allowSound }) {
 
   const [duration, setDuration] = useState(0);
 
-  // const playSound = useCallback(
-  //   function () {
-  //     if (!allowSound) return;
-  //     const sound = new Audio(clickSound);
-  //     sound.play();
-  //   },
-  //   [allowSound]
-  // );
-
   useEffect(
     function () {
       setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak);
@@ -37,7 +28,13 @@ function Calculator({ workouts, allowSound }) {
     [duration, allowSound]
   );
 
-  // const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
+  useEffect(
+    function () {
+      document.title = `Your ${number}-exersise workout`;
+    },
+    [number, duration, sets]
+  );
+
   const mins = Math.floor(duration);
   const seconds = (duration - mins) * 60;
 
